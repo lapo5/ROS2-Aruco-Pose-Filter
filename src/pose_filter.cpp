e20fde6eebc43f91110726d034c545d29f4db5ed
marcolapo5 <marco.lapolla5@gmail.com>
2021-11-18 14:39:41 +0100

    Update

--- src/pose_filter.cpp
+++ src/pose_filter.cpp
@@ -68,12 +68,7 @@ public:
     
     use_median = this->declare_parameter<bool>("median_filter.use_median", true);
 
-    if(use_median)
-      start_publishing = false;
-    else
-      start_publishing = true;
-
-    publish = false;
+    publish_data = false;
 
     float sampling_rate = this->declare_parameter<float>("butterworth_filter.sampling_rate", 10.0);
     float cutoff_frequency = this->declare_parameter<float>("butterworth_filter.cutoff_frequency", 1.0);
@@ -103,85 +98,106 @@ private:
 
   void pose_raw_callback_(const geometry_msgs::msg::TransformStamped::SharedPtr msg) {
 
+    last_N_measures_rx[counter] = msg->transform.rotation.x;
+    last_N_measures_ry[counter] = msg->transform.rotation.y;
+    last_N_measures_rz[counter] = msg->transform.rotation.z;
+    last_N_measures_rw[counter] = msg->transform.rotation.w;
+
     if (use_median)
     {
       last_N_measures_x[counter] = msg->transform.translation.x;
       last_N_measures_y[counter] = msg->transform.translation.y;
-      last_N_measures_z[counter] = msg->transform.translation.z;
-      last_N_measures_rx[counter] = msg->transform.rotation.x;
-      last_N_measures_ry[counter] = msg->transform.rotation.y;
-      last_N_measures_rz[counter] = msg->transform.rotation.z;
-      last_N_measures_rw[counter] = msg->transform.rotation.w;
-      counter = (counter + 1) % N_measures;
-
-      if (counter == 0)
-      {
-        start_publishing = true;
-      }
+      last_N_measures_z[counter] = msg->transform.translation.z;      
     }
-    else if(publish){
+    else if(publish_data){
       TransformStamped filtered_pose;
       filtered_pose.header.stamp = this->get_clock()->now();
       filtered_pose.header.frame_id = camera_link_frame;
       filtered_pose.child_frame_id = stable_link_frame_;
 
-      filtered_pose.transform.translation.x = (float) filters[0].filter(msg->transform.translation.x);
-      filtered_pose.transform.translation.y = (float) filters[1].filter(msg->transform.translation.y);
-      filtered_pose.transform.translation.z = (float) filters[2].filter(msg->transform.translation.z);
-      filtered_pose.transform.rotation.x = (float) filters[3].filter(msg->transform.rotation.x);
-      filtered_pose.transform.rotation.y = (float) filters[4].filter(msg->transform.rotation.y);
-      filtered_pose.transform.rotation.z = (float) filters[5].filter(msg->transform.rotation.z);
-      filtered_pose.transform.rotation.w = (float) filters[6].filter(msg->transform.rotation.w);
+      filtered_pose.transform.translation.x =  filters[0].filter(msg->transform.translation.x);
+      filtered_pose.transform.translation.y =  filters[1].filter(msg->transform.translation.y);
+      filtered_pose.transform.translation.z =  filters[2].filter(msg->transform.translation.z);
+
+      Eigen::Quaterniond q_temp;
+      Eigen::Quaterniond q_res;
+
+      int current_measure = counter;
+      q_res = Eigen::Quaterniond(last_N_measures_rw[current_measure], last_N_measures_rx[current_measure],
+              last_N_measures_ry[current_measure], last_N_measures_rz[current_measure]);
+
+      for(int i = 1; i < N_measures; i++){
+          current_measure = (counter - i) % N_measures;
+          if (current_measure < 0)
+            current_measure = current_measure + N_measures;
+
+          q_temp = Eigen::Quaterniond(last_N_measures_rw[current_measure], last_N_measures_rx[current_measure],
+              last_N_measures_ry[current_measure], last_N_measures_rz[current_measure]);
+
+          q_res = q_res.slerp(0.5, q_temp);
+      }
+      
+      filtered_pose.transform.rotation.x =  q_res.x();
+      filtered_pose.transform.rotation.y =  q_res.y();
+      filtered_pose.transform.rotation.z =  q_res.z();
+      filtered_pose.transform.rotation.w =  q_res.w();
       pose_filter_pub_->publish(filtered_pose);
 
       tf_broadcaster_->sendTransform(filtered_pose);
     }
+
+    counter = (counter + 1) % N_measures;
+    if (counter == 0)
+    {
+      publish_data = true;
+    }
+
   }
 
   void pose_presence_callback_(const std_msgs::msg::Bool::SharedPtr msg) {
 
       if(msg->data == true)
       {
-        this->publish = true;
+        this->publish_data = true;
       }
       else
-        this->publish = false;
+        this->publish_data = false;
   }
 
 
   void publish_filtering(){
-    if(use_median and start_publishing and publish){
-      TransformStamped filtered_pose;
-      filtered_pose.header.stamp = this->get_clock()->now();
-      filtered_pose.header.frame_id = camera_link_frame;
-      filtered_pose.child_frame_id = stable_link_frame_;
+    // if(use_median and publish_data){
+    //   TransformStamped filtered_pose;
+    //   filtered_pose.header.stamp = this->get_clock()->now();
+    //   filtered_pose.header.frame_id = camera_link_frame;
+    //   filtered_pose.child_frame_id = stable_link_frame_;
 
-      auto m = last_N_measures_x.begin() + last_N_measures_x.size()/2;
-      std::nth_element(last_N_measures_x.begin(), m, last_N_measures_x.end());
-      filtered_pose.transform.translation.x = (float) filters[0].filter(last_N_measures_x[last_N_measures_x.size()/2]);
+    //   auto m = last_N_measures_x.begin() + last_N_measures_x.size()/2;
+    //   std::nth_element(last_N_measures_x.begin(), m, last_N_measures_x.end());
+    //   filtered_pose.transform.translation.x =  filters[0].filter(last_N_measures_x[last_N_measures_x.size()/2]);
 
-      std::nth_element(last_N_measures_y.begin(), m, last_N_measures_y.end());
-      filtered_pose.transform.translation.y = (float) filters[1].filter(last_N_measures_y[last_N_measures_y.size()/2]);
+    //   std::nth_element(last_N_measures_y.begin(), m, last_N_measures_y.end());
+    //   filtered_pose.transform.translation.y =  filters[1].filter(last_N_measures_y[last_N_measures_y.size()/2]);
 
-      std::nth_element(last_N_measures_z.begin(), m, last_N_measures_z.end());
-      filtered_pose.transform.translation.z = (float) filters[2].filter(last_N_measures_z[last_N_measures_z.size()/2]);
+    //   std::nth_element(last_N_measures_z.begin(), m, last_N_measures_z.end());
+    //   filtered_pose.transform.translation.z =  filters[2].filter(last_N_measures_z[last_N_measures_z.size()/2]);
 
-      std::nth_element(last_N_measures_rx.begin(), m, last_N_measures_rx.end());
-      filtered_pose.transform.rotation.x = (float) filters[3].filter(last_N_measures_rx[last_N_measures_rx.size()/2]);
+    //   std::nth_element(last_N_measures_rx.begin(), m, last_N_measures_rx.end());
+    //   filtered_pose.transform.rotation.x =  filters[3].filter(last_N_measures_rx[last_N_measures_rx.size()/2]);
 
-      std::nth_element(last_N_measures_ry.begin(), m, last_N_measures_ry.end());
-      filtered_pose.transform.rotation.y = (float) filters[4].filter(last_N_measures_ry[last_N_measures_ry.size()/2]);
+    //   std::nth_element(last_N_measures_ry.begin(), m, last_N_measures_ry.end());
+    //   filtered_pose.transform.rotation.y =  filters[4].filter(last_N_measures_ry[last_N_measures_ry.size()/2]);
 
-      std::nth_element(last_N_measures_rz.begin(), m, last_N_measures_rz.end());
-      filtered_pose.transform.rotation.z = (float) filters[5].filter(last_N_measures_rz[last_N_measures_rz.size()/2]);
+    //   std::nth_element(last_N_measures_rz.begin(), m, last_N_measures_rz.end());
+    //   filtered_pose.transform.rotation.z =  filters[5].filter(last_N_measures_rz[last_N_measures_rz.size()/2]);
 
-      std::nth_element(last_N_measures_rw.begin(), m, last_N_measures_rw.end());
-      filtered_pose.transform.rotation.w = (float) filters[6].filter(last_N_measures_rw[last_N_measures_rw.size()/2]);
+    //   std::nth_element(last_N_measures_rw.begin(), m, last_N_measures_rw.end());
+    //   filtered_pose.transform.rotation.w =  filters[6].filter(last_N_measures_rw[last_N_measures_rw.size()/2]);
     
-      pose_filter_pub_->publish(filtered_pose);
+    //   pose_filter_pub_->publish(filtered_pose);
 
-      tf_broadcaster_->sendTransform(filtered_pose);
-    }
+    //   tf_broadcaster_->sendTransform(filtered_pose);
+    // }
     
   }
 
@@ -223,12 +239,11 @@ private:
 
   bool use_median;
 
-  bool start_publishing;
-  bool publish;
+  bool publish_data;
 
   static const int order = 3;
 
-  std::array<Iir::Butterworth::LowPass<order>, 7> filters;
+  std::array<Iir::Butterworth::LowPass<order>, 3> filters;
 
   
 };
